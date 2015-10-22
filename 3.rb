b931a211b4142acc8a4b0e003f556d318569fff0
dominikduda <gospodorz@gmail.com>
2015-10-22 16:00:27 +0200

    Added 1 solution

---
+++ 3.rb
@@ -0,0 +1,21 @@
+# https://projecteuler.net/problem=3
+# Answer is: 6857
+
+def prime?(number)
+  (2..number-1).none? { |i| number % i == 0}
+end
+
+def find_prime_factor(number)
+  n = 2
+  loop do
+    n == number ? break : nil
+    if prime?(n) && number % n == 0
+      number /= n
+      n = 2
+    end
+    n += 1
+  end
+  number
+end
+
+puts find_prime_factor(600851475143)
