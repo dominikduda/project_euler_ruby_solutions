a271fa3e5cdd5e9b618092c51ff362902b0ab1b2
dominikduda <gospodorz@gmail.com>
2015-10-27 01:26:33 +0100

    Refactored 14.rb

--- 14.rb
+++ 14.rb
@@ -1,17 +1,20 @@
+# https://projecteuler.net/problem=14
+# Answer is: 837799
+
 term_with_max_sequence_length = 0
 max_sequence_length = 0
 known_sequences = {}
-cache = []
-cache_length = 0
+temp_sequence_terms = []
 
-1.upto(3) do |starting_number|
+1.upto(999999) do |starting_number|
   next if known_sequences[starting_number]
-  sequence_length = 1
 
+  sequence_length = 1
   term = starting_number
+
   while term > 1
     unless known_sequences[term]
-      cache << term
+      temp_sequence_terms << term
       term.even? ? term /= 2 : term = 3 * term + 1
       sequence_length += 1
     else
@@ -20,11 +23,11 @@ cache_length = 0
     end
   end
 
-  cache.length.times do |i|
-    if known_sequences[cache[cache.length - i]]
-      cache.pop
+  temp_sequence_terms.length.times do |i|
+    if known_sequences[temp_sequence_terms[temp_sequence_terms.length - i]]
+      temp_sequence_terms.pop
     else
-      known_sequences[cache.pop] = (cache_length - cache.length) + ((sequence_length) - cache_length)
+      known_sequences[temp_sequence_terms.pop] = (sequence_length - temp_sequence_terms.length)
     end
   end
 
