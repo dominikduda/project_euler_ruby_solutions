667546c9ac74661a1b6233a44ed1027eddcf7dbc
dominikduda <gospodorz@gmail.com>
2015-11-07 11:45:03 +0100

    Refactored 22.rb

--- 22.rb
+++ 22.rb
@@ -1,7 +1,23 @@
 # https://projecteuler.net/problem=22
 # Answer is: 871198282
 
-module NamesOperations
+class Names
+  def initialize(names)
+    @names = names
+  end
+
+  def sum_of_names_scores
+    sum = 0
+    i = 1
+    to_sorted_array(@names).each do |name|
+      sum += alphabetical_value(name) * i
+      i += 1
+    end
+    sum
+  end
+
+  private
+
   def alphabetical_value(name)
     value = 0
     name.chars.each { |letter| value += letter.ord - 'a'.ord + 1 }
@@ -13,24 +29,6 @@ module NamesOperations
   end
 end
 
-class Names
-  include NamesOperations
-
-  def initialize(names)
-    @names = names
-  end
-
-  def sum_of_name_scores
-    sum = 0
-    i = 1
-    to_sorted_array(@names).each do |name|
-      sum += alphabetical_value(name) * i
-      i += 1
-    end
-    sum
-  end
-end
-
 names = File.open('materials/22.txt', 'r', &:read)
 names = Names.new(names)
-puts names.sum_of_name_scores
+puts names.sum_of_names_scores
